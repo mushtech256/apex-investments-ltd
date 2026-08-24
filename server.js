01c60c845367d1fe7556f6411e17ece59db5a74d
Your GitHub Username <your-email@example.com>
2026-08-24 15:09:23 +0300

    Update static path to root directory

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
+
 
 let users = [];
 
